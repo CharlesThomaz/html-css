ec1ea1a6d778d53ca8de738bfe92eb0bbb33454e
CharlesThomaz <charlesthomaz87@gmail.com>
2023-07-18 20:10:49 -0300

    Initial commit

---
+++ exercicios/ex015/app.js
@@ -0,0 +1,16 @@
+'user strict'
+
+const switcher = document.querySelector('.btn'); 
+
+switcher.addEventListener('click' function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = documente.body.className; 
+    if(className=="light-theme") {
+        this= textContent = "Dark";
+
+    }
+    else {
+        this.textContent = "Light";
+    }
+});
